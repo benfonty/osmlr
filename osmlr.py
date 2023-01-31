8f41fe90ecdd714f88e18834c1a8ed28974523c8
Your Name <benfonty@gmail.com>
2023-01-31 20:51:27 +0100

    further

--- osmlr.py
+++ osmlr.py
@@ -4,15 +4,15 @@ from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
 nominatim = Nominatim()
 overpass = Overpass()
 
-def getStreets(areaName):
+def getStreets(areaName, selector):
     areaId = nominatim.query(areaName).areaId()
-    query = overpassQueryBuilder(area=areaId, elementType='relation', selector='"type"="associatedStreet"', out='body')
+    query = overpassQueryBuilder(area=areaId, elementType='relation', selector='"type"="' + selector + '"', out='body')
     return overpass.query(query).relations()
 
 def getWay():
     return None
 
-streets = getStreets('Nantes, France')
+streets = getStreets('Nantes, France', 'street') + getStreets('Nantes, France', 'associatedStreet')
 print(len(streets))
 for street in streets:
     name = street.tags().get("name")
@@ -20,5 +20,5 @@ for street in streets:
     street.members()[0]._unshallow()
     print(street.members()[0].toXML())
   
-    wayIds = [ i.id() for i in street.members() ] # how to access the damn role to extract the street ways from the relation?
+    ways = [ i for i in street.members()] # how to access the damn role to extract the street ways from the relation?
     #print(name, wayIds)
