863115437ecc4bd714d3b8de05fad7245e57ca21
Your Name <benfonty@gmail.com>
2023-02-04 10:04:59 +0100

    works but strange results, lots of streets have length 0

--- osmlr.py
+++ osmlr.py
@@ -1,24 +1,47 @@
 from OSMPythonTools.nominatim import Nominatim
 from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
+import math
 
 nominatim = Nominatim()
 overpass = Overpass()
 
-def getStreets(areaName, selector):
+def getRelations(areaName, selector):
     areaId = nominatim.query(areaName).areaId()
     query = overpassQueryBuilder(area=areaId, elementType='relation', selector='"type"="' + selector + '"', out='body')
     return overpass.query(query).relations()
 
-def getWay():
-    return None
+def distance(lat1, lon1, lat2, lon2):
+    # https://www.movable-type.co.uk/scripts/latlong.html
+    R = 6371000
+    phi1 = lat1 * math.pi / 180
+    phi2 = lat2 * math.pi / 180
+    deltaPhi = (lat2 - lat1)  * math.pi / 180
+    deltaLambda = (lon2 - lon1)  * math.pi / 180
+    a = math.sin(deltaPhi/2) * math.sin(deltaPhi/2) + math.cos(phi1) * math.cos(phi2) * math.sin(deltaLambda/2) * math.sin(deltaLambda/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return R * c
 
-streets = getStreets('Nantes, France', 'street') + getStreets('Nantes, France', 'associatedStreet')
-print(len(streets))
-for street in streets:
-    name = street.tags().get("name")
-    #print(street.id())
-    street.members()[0]._unshallow()
-    print(street.members()[0].toXML())
-  
-    ways = [ i for i in street.members()] # how to access the damn role to extract the street ways from the relation?
-    #print(name, wayIds)
+def getLength(nodes):
+    if nodes == None or len(nodes) < 2:
+        return 0
+    result = 0
+    for i in range(len(nodes) - 1):
+        result += distance(nodes[i].lat(), nodes[i].lon(), nodes[i+1].lat(), nodes[i+1].lon())
+    return result
+    
+
+streetRelations = getRelations('Nantes, France', 'street') + getRelations('Nantes, France', 'associatedStreet')
+print("found", len(streetRelations), "streets")
+for relation in streetRelations:
+    name = relation.tags().get("name")
+    streetLength = 0
+    for member in relation.members():
+            if "highway" in member.tags():
+                length = getLength(member.nodes())
+                if length == 0:
+                    print("WARN, a way of", name, "is empty")
+                streetLength += length
+    print("\t", "street name", name, "has", len(relation.members()), "members and length", streetLength)
+
+
+
